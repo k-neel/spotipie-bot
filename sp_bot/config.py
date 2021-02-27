734dfd6d4fed8032ef923c9b69466eec1b96bbc5
neel1999 <neelkanthkapil1@gmail.com>
2021-02-27 16:47:49 +0530

    test

--- sp_bot/config.py
+++ sp_bot/config.py
@@ -7,7 +7,7 @@ class Config:
     SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
     SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
     # MONGODB_URI = os.getenv('MONGODB_URI')
-    OWNER_USERNAME = '@neelplaysac'
+    OWNER_USERNAME = ''
     MONGO_USR = os.getenv('MONGO_USR')
     MONGO_PASS = os.getenv('MONGO_PASS')
     MONGO_COLL = os.getenv('MONGO_COLL')
